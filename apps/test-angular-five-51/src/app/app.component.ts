fd2f7a1ddffdc7af685aa1d52a8b3a5378133d75
Charlie Greenman <charlie@razroo.com>
2023-09-19 15:05:02 -0400

    Create starter files for angular-16.2.0

---
+++ apps/test-angular-five-51/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-angular-five-51-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-angular-five-51';
+}
